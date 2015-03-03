f19c4ed01b114770a86c520bdcc734e6468c3bb3
Ryan Varley <ryanjvarley@gmail.com>
2015-03-03 15:07:48 +0000

    working pdf calculations with tests, changed format of grim class to work as a calculator for that grim, dettached from the observation

--- grism.py
+++ grism.py
@@ -16,53 +16,56 @@ import params
 
 
 class Grism(object):
+    """ Handles a grism object and can be used to peform calculation on it such as the psf given a flux and wavelength
 
-    def __init__(self, flux, wavelength):
-        """ Takes input of a spectrum of flux and wavelength, it can then rebin
+    This class should not include observation data and instead be used to turn observational dat into observed. An
+    observation / combined detector grism class can do this. calling each component as its needed. You are not then doing
 
-        :param flux: list of fluxes of the raw spectrum
-        :type flux: list / numpy.ndarray
-        :param wavelength: list of wavelengths of the raw spectrum
-        :type wavelength: list / numpy.ndarray
-        """
-        self.raw_flux = flux  # TODO should have units
-        self.raw_wavelength = wavelength
+    x = grism(observation, g141)
+    x.psf(wl)
+
+    but x.psf(wl, flux), psf should include all the factors, such as throughput
+    """
+    def __init__(self):
+        """ In future will take the vars that define a unique grism, for now this is kept WFC3 G141 specific, with all
+        params defined in __init__. this is mostly because i dont know what unique set of params will be required in the
+        end for each grism.
 
         self.detector = WFC3_IR()  # this should be called and set by an observation / instrument class
+        Remeber many parameters like dispersion here are intrinsically linked to the detector, so this is really
+        WFC3 G141 not G141 applicable to any instrument.
+        """
+
+        # Detector Values
+        # ---------------
+        # Note that most det values are being pulled directly from the detector class. Given that these two classes are
+        # intrinsically linked this is probably ok, but can be changed if needed. (i.e. self.detector.pixel_unit)
+        self.detector = WFC3_IR()
 
         # Grism Values
+        # ------------
         self.min_lambda = 1075 * pq.nm
         self.max_lambda = 1700 * pq.nm
 
-        self.dispersion = 4.65 * pq.nm
+        # self.dispersion = 4.65 * pq.nm - The dispersion is actually dependant on x and y and not constant
 
+        ## PSF Information
         self._psf_file = np.loadtxt(os.path.join(params._data_dir, 'wfc3-g141-fwhm.dat'))
         # in future add option to set unit of data file but we work internally in microns
         self.psf_wavelength = (self._psf_file[:, 0] * pq.nm).rescale(pq.micron)
         self.psf_fwhm = self._psf_file[:, 1] * self.detector.pixel_unit
 
-        # Some constants
+        # Non Grism Specific Constants
         self._FWHM_to_StDev = 1./(2*np.sqrt(2*np.log(2)))
-
-    def rebin(self):
-        """ Rebins the raw spectrum to the resolution of the grism.
-
-        .. Note:
-            * due to the way the grism is mapped on the detector, a closer relationship between grism and detector
-            may be needed and this function changed when angled.
-
-        :return: rebinned spectrum (pixel units)
-        """
-
-        # TODO rebin using pysynphot
 
     def throughput(self):
         """ should be done before the binning, the convert each flux and $\lambda$ to the correct throughput
-        :return:
+
+        :return: factor to scale the flux by
         """
         pass
 
-    def psf(self, wavelength, flux):
+    def flux_to_psf(self, wavelength, flux):
         """
         Given a wavelength and flux this function returns the gaussian function for the observation at the wl given
          (linearly interpolated using numpy.interp)
@@ -72,11 +75,16 @@ class Grism(object):
         We assume the psf can be represented by a gaussian, this is true for WFC3 G141 (*WFC3 inst handbook (cycle 23)
          - sec 7.6.1 - page 140*).
 
-        :param wavelength: wavelength to sample in microns
+        :param wavelength: wavelength to sample (quantites unit length)
         :param flux: The flux at the wavelength (i.e the area under the gaussian)
 
         :return:
         """
+
+        if not type(wavelength) is pq.quantity.Quantity:
+            raise TypeError("Wavelength must be given as a quantities unit i.e. 1 * pq.micron got {} type {}".format(
+                wavelength, type(wavelength)
+            ))
 
         mean = 0.  # this is the center, make sense that we zero it for now
 
--- tests/test_grism.py
+++ tests/test_grism.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import quantities as pq
 
 from .. import grism
 
@@ -11,18 +12,36 @@ class Test_Grism(unittest.TestCase):
 
         self.flux = np.linspace(0.9, 1.2, 10)
         self.wavelength = np.linspace(0.9, 1.2, 10)
-        self.g141_grism = grism.Grism(self.flux, self.wavelength)
+        self.g141_grism = grism.Grism()
 
-    def test_psf_at_sample_points(self):
+    def test__init__(self):
+        g141_grism = grism.Grism()  # pass if no exceptions
 
-        # self.g141_grism.psf(1.2)
-        # self.g141_grism.psf(1.7)
+    def test_flux_to_psf_raises_TypeError_given_no_units(self):
+        with self.assertRaises(TypeError):
+            self.g141_grism.flux_to_psf(1.2, 1.)
 
-        assert False
+    def test_flux_to_psf_at_sample_points(self):
+        # Verified using MCMC
+        psf1 = self.g141_grism.flux_to_psf(1.2 * pq.micron, 1.)
+        self.assertAlmostEqual(psf1.flux, 1., 9)
+        self.assertAlmostEqual(psf1.amplitude, 0.9033, 4)
+        self.assertAlmostEqual(psf1.stddev, 0.4416, 4)
 
-    def test_psf_between_sample_points_interpolation(self):
-        #
-        # self.g141_grism.psf(1.15)
-        # self.g141_grism.psf(1.65)
+        psf2 = self.g141_grism.flux_to_psf(1.7 * pq.micron, 1.5)
 
-        assert False
+        self.assertAlmostEqual(psf2.flux, 1.5, 9)
+        self.assertAlmostEqual(psf2.amplitude, 1.1560, 4)
+        self.assertAlmostEqual(psf2.stddev, 0.5177, 4)
+
+    def test_flux_to_psf_between_sample_points_interpolation(self):
+
+        psf1 = self.g141_grism.flux_to_psf(1.15 * pq.micron, 1.)
+        self.assertAlmostEqual(psf1.flux, 1., 9)
+        self.assertAlmostEqual(psf1.amplitude, 0.9125, 4)
+        self.assertAlmostEqual(psf1.stddev, 0.4372, 4)
+
+        psf2 = self.g141_grism.flux_to_psf(1.65 * pq.micron, 1.5)
+        self.assertAlmostEqual(psf2.flux, 1.5, 9)
+        self.assertAlmostEqual(psf2.amplitude, 1.1767, 4)
+        self.assertAlmostEqual(psf2.stddev, 0.5085, 4)
