43b3b0c545b972b099082f682408f81dd8e8d6b2
Ryan Varley <ryanjvarley@gmail.com>
2015-10-01 19:49:28 +0100

    added EASE data quality function

--- tools.py
+++ tools.py
@@ -193,3 +193,203 @@ def get_limb_darkening_coeffs(star):
     """
 
     return pylc.ldcoeff(star.Z, float(star.T), star.calcLogg(), 'J')
+
+
+class WFC3IR_DQFlags(object):
+    """ This class analyses a single data quality flag from the WFC3 IR camera.
+    This includes general problem checking ie DQFlags.problems() aswell as
+    looking for individual problems ie .isBad(), .isHot() etc.
+
+    This class is from the EASE Pipeline
+
+    Flags are WFC3 IR specific from 'WFC3 data handbook 2.1'
+    """
+
+    def __init__(self, flag):
+        """
+        :param flag: DQ flag
+        :type flag: int
+        """
+
+        self.flag = flag
+        self.binary = '{0:015b}'.format(flag)
+
+        self._flagList()  # sets flagLists
+
+    def isBad(self):  # Notes as self.binary is a str then all slices are also str
+        """ Checks weather the pixel is bad by seeing if ANY flags are raised
+        :return: True or False
+        :rtype: bool
+        """
+        print self.binary, type(self.binary)
+        if '1' in self.binary:
+            return True
+        else:
+            return False
+
+    def isRSError(self):
+        if self.binary[14] is '1':
+            return True
+        else:
+            return False
+
+    def isFilled(self):
+        if self.binary[13] is '1':
+            return True
+        else:
+            return False
+
+    def isBadPix(self):
+        if self.binary[12] is '1':
+            return True
+        else:
+            return False
+
+    def isDeviantZero(self):
+        if self.binary[11] is '1':
+            return True
+        else:
+            return False
+
+    def isHot(self):
+        if self.binary[10] is '1':
+            return True
+        else:
+            return False
+
+    def isUnstable(self):
+        if self.binary[9] is '1':
+            return True
+        else:
+            return False
+
+    def isWarm(self):
+        if self.binary[8] is '1':
+            return True
+        else:
+            return False
+
+    def isBadRef(self):
+        if self.binary[7] is '1':
+            return True
+        else:
+            return False
+
+    def isSaturated(self):
+        if self.binary[6] is '1':
+            return True
+        else:
+            return False
+
+    def isBadFlat(self):
+        if self.binary[5] is '1':
+            return True
+        else:
+            return False
+
+    def isReserved(self):
+        if self.binary[4] is '1':
+            return True
+        else:
+            return False
+
+    def isZeroSignal(self):
+        if self.binary[3] is '1':
+            return True
+        else:
+            return False
+
+    def isCosmic(self):
+        """ there are two methods for checking cosmics, this returns True if either raise the flag
+        :return:
+        """
+        if self.binary[2] is '1' or self.binary[1] is '1':
+            return True
+        else:
+            return False
+
+    def isCosmic_drizzle(self):
+        if self.binary[2] is '1':
+            return True
+        else:
+            return False
+
+    def isCosmic_calwf3(self):
+        if self.binary[1] is '1':
+            return True
+        else:
+            return False
+
+    def isGhost(self):
+        if self.binary[0] is '1':
+            return True
+        else:
+            return False
+
+    def problems(self):
+        """ returns a list of all the problem flags
+
+        :return: list of flag shortnames
+        :rtype: list
+        """
+        problems = []
+        for i, num in enumerate(list(self.binary)):
+            if num is '1':
+                problems.append(self.flagList[i][1])
+
+        return problems
+
+    def _isFlag(self, flag):  # TODO rewrite and test this (currently tested by data)
+        """ Takes a flag or tuple/list of flags as input and returns True if any of the flags are raised and false if
+        none are
+
+        :return: True / False
+        :rtype: bool
+        """
+
+        flagList = self.flagList + self.extraFlags
+
+        for flagInfo in flagList:  # Not the most efficient way but good enough
+            flagNum = flagInfo[0]
+            flagCode = flagInfo[1]
+            if flag == flagCode:
+                if type(flagNum) is int:
+                    if self.binary[flagNum] is '1':
+                        return True
+                    else:
+                        return False
+                else:
+                    for flagNumber in flagNum:  # In this case there are multiple flagnums in flagnum
+                        if self.binary[flagNumber] is '1':
+                            return True
+
+                    return False  # if we're here no flags matched in the list
+
+
+        raise ValueError('{} is not a valid flag'.format(flag))
+
+    def _flagList(self):
+        """ just an list of flags in order from binary 16384 to 0 (ie backwards) with flags grouped in extra flags
+        function sets them in the class
+        """
+
+        self.flagList = [  # Format - position of flag / flag shortname / flag description
+            (0, 'ghost', 'ghost/crosstalk'),
+            (1, 'cosmic-calwf3', 'cosmic ray (calwf3 up-the-ramp fitting)'),
+            (2, 'cosmic-drizzle', 'cosmic ray (MultiDrizzle)'),
+            (3, 'zero-signal', 'Signal in zero-read'),
+            (4, 'reserved', '(Reserved)'),
+            (5, 'badflat', 'Bad or uncertain flat value'),
+            (6, 'saturated', 'Full-well saturation'),
+            (7, 'badref', 'Bad reference pixel'),
+            (8, 'warm', 'Warm pixel'),
+            (9, 'unstable', 'Unstable response'),
+            (10, 'hot', 'Hot pixel'),
+            (11, 'deviantzero', 'Deviant zero-read (bias) value'),
+            (12, 'badpix', 'Bad detector pixel'),
+            (13, 'filled', 'Data missing and replaced by fill value'),
+            (14, 'reedsolomon', 'Reed-Solomon decoding error'),
+        ]
+        self.extraFlags = [  # combines flags into more general flags
+            ((1, 2), 'cosmic', 'Cosmic ray')
+        ]
