b51ff256c3f0bbc6eb93f415a62edbac15d62a6a
Ryan Varley <ryanjvarley@gmail.com>
2015-11-12 13:33:49 +0000

    PSF distribution is now 3x faster

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 """
 
 __author__ = 'Ryan Varley'
-__version__ = '0.6b.151111a'
+__version__ = '0.6b.151112a'
 __short_version__ = '0.6b'
 __all__ = ('detector', 'observation' 'exposure', 'grism', 'models', 'params', 'tools')
 
